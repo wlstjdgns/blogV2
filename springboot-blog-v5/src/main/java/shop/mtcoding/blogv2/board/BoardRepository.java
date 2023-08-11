91c7e72a9a95afc2f182987561c8b546380cd3d0
wlstjdgns <jin009471@naver.com>
2023-08-11 14:15:47 +0900

    boardService 글쓰기 구현완료

--- springboot-blog-v5/src/main/java/shop/mtcoding/blogv2/board/BoardRepository.java
+++ springboot-blog-v5/src/main/java/shop/mtcoding/blogv2/board/BoardRepository.java
@@ -1,8 +1,13 @@
 package shop.mtcoding.blogv2.board;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-@Repository
-public class BoardRepository {
+/*
+* 레파지토리 어노테이션 안해도 컴포넌트 스캔이 된다.
+* save(), findById(), findAll(), count(), deleteById() 
+* 기본 CRUD 다 만들어져 있다.(update 빼고)
+*/
+public interface BoardRepository extends JpaRepository<Board, Integer> {
 
 }
